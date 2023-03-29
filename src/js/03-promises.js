58e358cf74c6aafc5b91a959837bcbd2861d9d1c
Андрій <109473689+Andriy-Minyailo@users.noreply.github.com>
2023-03-29 20:40:34 +0300

    test3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,17 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener("submit", oncreatePromise);
+
+
+function oncreatePromise(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget.elements;
+  
+}
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
