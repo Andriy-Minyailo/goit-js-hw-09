5bdb1ba6b8d4a9ba2160737ce9bb648f97793145
Андрій <109473689+Andriy-Minyailo@users.noreply.github.com>
2023-03-31 12:43:19 +0300

    task3 final

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,15 +8,51 @@ form.addEventListener("submit", oncreatePromise);
 function oncreatePromise(event) {
   event.preventDefault();
   const { delay, step, amount } = event.currentTarget.elements;
-  
+  if (!(delay.value >= 0 && step.value >= 0 && amount.value > 0)) {
+    Notify.warning('Please, enter valid data!')
+    return;
+  }
+  let resultDelay = Number(delay.value);
+  let position = 1;
+  setTimeout(() => {
+    createPromise(position, resultDelay).then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    const idInteval = setInterval(() => {
+          if (position === Number(amount.value)) { 
+          clearInterval(idInteval);
+          return;
+      }
+      resultDelay += Number(step.value);
+      position += 1;
+        createPromise(position, resultDelay).then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+          .catch(({ position, delay }) => {
+            Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+            console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+          })
+       }, step.value); 
+    }, delay.value)
 }
 
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
   } else {
-    // Reject
-  }
-}
+      reject({ position, delay });
+  } 
+  })
+};
+
+
